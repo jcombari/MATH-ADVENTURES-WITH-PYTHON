51595f915cd4aa568405c71942c77c58b82066c4
jcombari <jcombari@gmail.com>
2022-02-18 13:19:15 +0100

    updating

---
+++ cap01_square.py
@@ -0,0 +1,18 @@
+from turtle import *
+# loop for number of squares
+for i in range(60):
+      
+    # loop for drawing each square
+    for j in range(4):
+          
+        # drawing each side of
+        # square of length 100 
+        fd(100)
+          
+        # turning 90 degrees
+        # to the right
+        rt(5)
+          
+    # turning 6 degrees for
+    # the next square
+    rt(6)
